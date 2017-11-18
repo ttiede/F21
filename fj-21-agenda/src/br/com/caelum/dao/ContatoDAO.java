7f8b7998ad6a47cf106429966ca64e62b2d5b633
Tiago Tiede <web6956@caelum141-11>
2017-11-18 10:43:08 -0200

    Create Contato by Servlet

--- fj-21-agenda/src/br/com/caelum/dao/ContatoDAO.java
+++ fj-21-agenda/src/br/com/caelum/dao/ContatoDAO.java
@@ -15,85 +15,33 @@ import br.com.caelum.vo.ContatoVO;
 
 public class ContatoDAO {
 
-	public void insert(ContatoVO contato1) {
-
-		String sql = "INSERT INTO Contato (Nome,Email,Endereco,DataNascimento) values (?,?,?,?)";
-		Connection connection = null;
-
-		PreparedStatement stmt;
-
-		try {
-			connection = ConnectionFactory.getConnection();
-			stmt = connection.prepareStatement(sql);
-			long dataNascimentoMileSegundos = contato1.getDataNascimento().getTimeInMillis();
-
-			stmt.setString(1, contato1.getNome());
-			stmt.setString(2, contato1.getEmail());
-			stmt.setString(3, contato1.getEndereco());
-			stmt.setDate(4, new Date(dataNascimentoMileSegundos));
-
-			stmt.execute();
-
-			stmt.close();
-
-		} catch (SQLException e) {
-			throw new DAOException(null, e);
-		} finally {
-			closeConnection(connection);
-		}
+	public void insert(ContatoVO contato) {
+		final String sql = "INSERT INTO Contato (Nome,Email,Endereco,DataNascimento) values (?,?,?,?)";
+		this.mergeData(contato, sql);
 	}
 
-	public void update(ContatoVO contato1) {
-		Connection connection = null;
-		String sql = "UPDATE Contato SET Nome = ? ,Email = ? ,Endereco = ? ,DataNascimento = ? WHERE ContatoID = ?";
-
-		try {
-			connection = ConnectionFactory.getConnection();
-			PreparedStatement stmt = connection.prepareStatement(sql);
-
-			long dataNascimentoMileSegundos = contato1.getDataNascimento().getTimeInMillis();
-
-			stmt.setString(1, contato1.getNome());
-			stmt.setString(2, contato1.getEmail());
-			stmt.setString(3, contato1.getEndereco());
-			stmt.setDate(4, new Date(dataNascimentoMileSegundos));
-			stmt.setLong(5, contato1.getContatoID());
-
-			stmt.execute();
-
-			stmt.close();
-		} catch (SQLException e) {
-			throw new DAOException(null, e);
-		} finally {
-			closeConnection(connection);
-		}
+	public void update(final ContatoVO contato) {
+		final String sql = "UPDATE Contato SET Nome = ? ,Email = ? ,Endereco = ? ,DataNascimento = ? WHERE ContatoID = ?";
+		this.mergeData(contato, sql);
 	}
 
-	public List<ContatoVO> getContatos(String where) {
-		List<ContatoVO> contatos = new ArrayList<>();
+	public List<ContatoVO> getContatos(final String where) {
+		final List<ContatoVO> contatos = new ArrayList<>();
 		Connection connection = null;
 		try {
 			connection = ConnectionFactory.getConnection();
-			PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Contato");
 
-			if (where != null) {
+			final PreparedStatement stmt;
+			if (where == null) {
+				stmt = connection.prepareStatement("SELECT * FROM Contato");
+			} else {
 				stmt = connection.prepareStatement("SELECT * FROM Contato WHERE ?");
 				stmt.setString(1, String.valueOf(where));
 			}
 
-			ResultSet rs = stmt.executeQuery();
-			
+			final ResultSet rs = stmt.executeQuery();
 			while (rs.next()) {
-				ContatoVO contato = new ContatoVO();
-				contato.setContatoID(rs.getLong("ContatoID"));
-				contato.setNome(rs.getString("Nome"));
-				contato.setEmail(rs.getString("Email"));
-				contato.setEndereco(rs.getString("Endereco"));
-
-				Calendar data = Calendar.getInstance();
-				data.setTime(rs.getDate("DataNascimento"));
-				contato.setDataNascimento(data);
-				contatos.add(contato);
+				contatos.add(this.buildContato(rs));
 			}
 			rs.close();
 			stmt.close();
@@ -102,73 +50,95 @@ public class ContatoDAO {
 		} catch (SQLException e) {
 			throw new DAOException(null, e);
 		} finally {
-			closeConnection(connection);
+			this.closeConnection(connection);
 		}
 	}
 
-	public List<ContatoVO> getById(Integer id) {
-		List<ContatoVO> contatos = new ArrayList<>();
+	public List<ContatoVO> getById(final Integer id) {
+		final List<ContatoVO> contatos = new ArrayList<>();
 		Connection connection = null;
 		try {
-			PreparedStatement stmt = null;
 			String querySearch = "SELECT * FROM Contato WHERE ContatoID = ?";
 			if (id != null) {
 				connection = ConnectionFactory.getConnection();
-				stmt = connection.prepareStatement(querySearch);
+
+				final PreparedStatement stmt = connection.prepareStatement(querySearch);
 				stmt.setLong(1, id);
-			}
 
-			ResultSet rs = stmt != null ? stmt.executeQuery() : null;
-			while (rs != null ? rs.next() : false) {
-				ContatoVO contato = new ContatoVO();
-				contato.setContatoID(rs.getLong("ContatoID"));
-				contato.setNome(rs.getString("Nome"));
-				contato.setEmail(rs.getString("Email"));
-				contato.setEndereco(rs.getString("Endereco"));
+				final ResultSet rs = stmt.executeQuery();
 
-				Calendar data = Calendar.getInstance();
-				data.setTime(rs.getDate("DataNascimento"));
-				contato.setDataNascimento(data);
-				contatos.add(contato);
+				while (rs != null ? rs.next() : false) {
+					contatos.add(this.buildContato(rs));
+				}
+				rs.close();
+				stmt.close();
 			}
-			assert rs != null;
-			rs.close();
-			stmt.close();
 			return contatos;
-
 		} catch (SQLException e) {
 			throw new DAOException(null, e);
 		} finally {
-			closeConnection(connection);
+			this.closeConnection(connection);
 		}
 	}
 
-	public String deleteById(Long id) {
+	private ContatoVO buildContato(final ResultSet rs) throws SQLException {
+		ContatoVO contato = new ContatoVO();
+		contato.setContatoID(rs.getLong("ContatoID"));
+		contato.setNome(rs.getString("Nome"));
+		contato.setEmail(rs.getString("Email"));
+		contato.setEndereco(rs.getString("Endereco"));
+
+		Calendar data = Calendar.getInstance();
+		data.setTime(rs.getDate("DataNascimento"));
+		contato.setDataNascimento(data);
+		return contato;
+	}
+
+	public String deleteById(final Long id) {
 		Connection connection = null;
 		try {
 			connection = ConnectionFactory.getConnection();
 
-			PreparedStatement stmt = null;
 			String queryDelete = "DELETE FROM Contato WHERE ContatoID = ?";
 			if (id != null) {
-				stmt = connection.prepareStatement(queryDelete);
-
+				final PreparedStatement stmt = connection.prepareStatement(queryDelete);
 				stmt.setLong(1, id);
-
 				stmt.execute();
-
 				stmt.close();
-				return "Contato excluído:" + id;
+				return String.format("Contato excluído: %s", id);
 			}
 		} catch (SQLException e) {
-			throw new DAOException("falha ao excluir contato:" + id, e);
+			throw new DAOException(String.format("falha ao excluir contato: %s",id), e);
 		} finally {
-			closeConnection(connection);
+			this.closeConnection(connection);
 		}
 		return null;
 	}
 
-	private void closeConnection(Connection connection) {
+	private void mergeData(final ContatoVO contatoVO, final String qry) {
+		Connection connection = null;
+
+		try {
+			connection = ConnectionFactory.getConnection();
+			long dataNascimentoMileSegundos = contatoVO.getDataNascimento().getTimeInMillis();
+
+			final PreparedStatement stmt = connection.prepareStatement(qry);
+			stmt.setString(1, contatoVO.getNome());
+			stmt.setString(2, contatoVO.getEmail());
+			stmt.setString(3, contatoVO.getEndereco());
+			stmt.setDate(4, new Date(dataNascimentoMileSegundos));
+
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new DAOException(null, e);
+		} finally {
+			this.closeConnection(connection);
+		}
+
+	}
+
+	private void closeConnection(final Connection connection) {
 		if (connection != null) {
 			try {
 				connection.close();
--- fj-21-agenda/src/br/com/caelum/util/DateUtil.java
+++ fj-21-agenda/src/br/com/caelum/util/DateUtil.java
@@ -1,18 +1,21 @@
 package br.com.caelum.util;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 public class DateUtil {
-	public static String dateFormatDayMonthYear(final String date){
-		 return new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(date);
+	public static String dateFormatDayMonthYear(final Calendar dateCalendar){
+		 DateFormat dateFormat =  new SimpleDateFormat("dd/MM/yyyy");
+		 return dateFormat.format(dateCalendar.getTime());
 	}
 
-	public static Date dateParseDayMonthYear(final String date){
+	public static Date dateParseDayMonthYear(final String dateString){
 		 try {
-			return new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").parse(date);
+			final Date date =new SimpleDateFormat("dd/MM/yyyy").parse(dateString);
+			return date ;
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
@@ -21,7 +24,7 @@ public class DateUtil {
 
 	public static Calendar stringToDate(final String dateString){
 		final Calendar dataNascimento = Calendar.getInstance();
-		Date date = dateParseDayMonthYear(dateString);
+		final Date date = dateParseDayMonthYear(dateString);
 		dataNascimento.setTime(date);
 		return dataNascimento;
 	}
